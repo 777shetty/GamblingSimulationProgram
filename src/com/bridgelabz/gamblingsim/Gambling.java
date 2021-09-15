9e4a504a9c9ee8166a2ec5cbc7c2d9f2ae0b011a
777shetty <shetty.chetu7@gmail.com>
2021-09-15 12:47:30 +0530

    ADDED:Termination for the condition

--- src/com/bridgelabz/gamblingsim/Gambling.java
+++ src/com/bridgelabz/gamblingsim/Gambling.java
@@ -4,7 +4,7 @@ public class Gambling {
 	public static final int STAKE = 100;
 	public static int GAMBLER_STAKE = 100;
 	public static final int DAY_BETTING_PRICE = 1;
-	public void play() {
+	public boolean play() {
 
 		if((Math.random()*100)>50) {
 			System.out.println("Win");
@@ -14,6 +14,11 @@ public class Gambling {
 			GAMBLER_STAKE -= 1;
 
 		}
+		if (GAMBLER_STAKE > 150 || GAMBLER_STAKE < 50)
+			
+			return false;
+		else
+			return true;
 	}
 	
 	public static void main(String[] args) {
@@ -22,6 +27,7 @@ public class Gambling {
 		Gambling gambler = new Gambling();
 		System.out.println("Stake:"+ gambler.STAKE);
 		System.out.println("Per day betting price:"+ gambler.DAY_BETTING_PRICE);
+		while (gambler.play());
 
 	}
 }
